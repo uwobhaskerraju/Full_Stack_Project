9893f03095c1bd24964327171ae2414aa087b4a7
uwobhaskerraju <schittap@uwo.ca>
2019-11-24 12:58:06 -0500

    initial commit - adding images, dbscripts , APIs

---
+++ app/controllers/library.controller.js
@@ -0,0 +1,26 @@
+// const Note = require('../models/note.model.js');
+
+
+exports.validateUser = (req, res) => {
+    
+};
+
+// Retrieve and return all notes from the database.
+exports.registerUser = (req, res) => {
+console.log("")
+};
+
+// Find a single note with a noteId
+exports.findOne = (req, res) => {
+
+};
+
+// Update a note identified by the noteId in the request
+exports.update = (req, res) => {
+
+};
+
+// Delete a note with the specified noteId in the request
+exports.delete = (req, res) => {
+
+};
---
+++ app/models/note.model.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+
+const NoteSchema = mongoose.Schema({
+    title: String,
+    content: String
+}, {
+    timestamps: true
+});
