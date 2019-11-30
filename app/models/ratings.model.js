87fa0b7a3361a16bbacfd2d6ca63fcf654c096ee
uwobhaskerraju <schittap@uwo.ca>
2019-11-30 18:27:41 -0500

    adding ratings model

---
+++ app/models/ratings.model.js
@@ -0,0 +1,11 @@
+var mongoose     = require('mongoose');
+
+var ReviewSchema   = mongoose.Schema({
+    songID: { type: String, required: true },
+    userName:{ type: String, required: true },
+    ratings:{ type: Number, required: true }
+}, {
+    versionKey: false // You should be aware of the outcome after set to false
+});
+
+module.exports = mongoose.model('Ratings', ReviewSchema,'Ratings');
