05aae10dd09b4614d8d14b2fd123f6199b4f0b3e
uwobhaskerraju <schittap@uwo.ca>
2019-12-01 00:11:52 -0500

    adding hidden field to playlist model

--- app/models/playlist.model.js
+++ app/models/playlist.model.js
@@ -1,13 +1,14 @@
-var mongoose     = require('mongoose');
+var mongoose = require('mongoose');
 
-var PlaylistSchema   = mongoose.Schema({
+var PlaylistSchema = mongoose.Schema({
     _id: { type: mongoose.Schema.ObjectId, auto: true },
     title: { type: String, required: true },
-    description:{ type: String, required: true },
-    ownerID:{ type: mongoose.Schema.ObjectId, required: true },
-    songID:{ type: [mongoose.Schema.ObjectId], required: true }
+    description: { type: String, required: true },
+    ownerID: { type: mongoose.Schema.ObjectId, required: true },
+    songID: { type: [mongoose.Schema.ObjectId], required: true },
+    hidden: { type: Boolean, default: false }
 }, {
     versionKey: false // You should be aware of the outcome after set to false
 });
 
-module.exports = mongoose.model('Playlists', PlaylistSchema,'Playlists');
+module.exports = mongoose.model('Playlists', PlaylistSchema, 'Playlists');
