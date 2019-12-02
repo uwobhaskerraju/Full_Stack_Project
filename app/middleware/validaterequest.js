8e44b0ffc9974923ce01e2d429f02f166e94e4b6
uwobhaskerraju <schittap@uwo.ca>
2019-12-01 23:18:25 -0500

    adding payload validation for user registration

--- app/middleware/validaterequest.js
+++ app/middleware/validaterequest.js
@@ -1,4 +1,22 @@
-module.exports=(req,res,next)=>{
-console.log('inside middle')
-next();
+function generateKeyValueFromBody(body) {
+    const entries = Object.keys(body)
+    const inserts = {}
+    for (let i = 0; i < entries.length; i++) {
+        inserts[entries[i]] = Object.values(body)[i]
+    }
+    return inserts;
+}
+
+const errMsg='[INVALID] Not a valid request'
+
+module.exports = (req, res, next) => {
+
+    var exports = generateKeyValueFromBody(req.body)
+    console.log(exports)
+    if(!exports.email) return res.send({message:errMsg})
+    if(!exports.username) return res.send({message:errMsg})
+    if(!exports.password) return res.send({message:errMsg})
+    //write validation code here
+    next();
+
 };
