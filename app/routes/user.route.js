3b3099bb6cee2496b0933c47142aeb12ddc1e7ff
uwobhaskerraju <schittap@uwo.ca>
2019-12-01 23:57:46 -0500

    adding middleware checks for user registration

--- app/routes/user.route.js
+++ app/routes/user.route.js
@@ -1,6 +1,8 @@
 module.exports = (app) => {
     const user = require('../controllers/user.controller.js');
+    const checkrequest=require('../middleware/validaterequest.js');
+    
     //register user
-    app.post('/open/register',user.registerUser);
+    app.post('/user/register',checkrequest,user.registerUser);
 
 }
